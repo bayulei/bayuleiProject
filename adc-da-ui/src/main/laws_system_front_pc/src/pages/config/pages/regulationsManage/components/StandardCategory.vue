a3dc2c84ec02784d47d1259197cdbdb1d1e81ee3
chenxiaoxi <chenxiaoxi@91isoft.com>
2018-09-04 10:48:41 +0800

    构建配置管理基础页面

--- adc-da-ui/src/main/laws_system_front_pc/src/pages/config/pages/regulationsManage/components/StandardCategory.vue
+++ adc-da-ui/src/main/laws_system_front_pc/src/pages/config/pages/regulationsManage/components/StandardCategory.vue
@@ -5,17 +5,14 @@
       <div slot="left">
         <Input v-model="keywords1" placeholder="根据用户名查找" clearable class="my-input" />
         <Input v-model="keywords2" placeholder="根据描述查找" clearable class="my-input" />
-        <Button type="primary" icon="ios-search" :loading="searching"></Button>
+        <Button type="primary" icon="ios-search" :loading="searching" @click="searchData"></Button>
       </div>
       <div slot="right">
         <Button type="primary" @click="isAdvancedSearch = true">高级检索</Button>
       </div>
     </table-tools-bar>
     <div class="content">
-      <Spin fix>
-        <Icon custom="i-icon icon-icon-test" size=18 class="demo-spin-icon-load"></Icon>
-        <div>Loading</div>
-      </Spin>
+      <loading :loading="loading">数据获取中</loading>
       <Table border ref="selection" :columns="tableColumn" :data="data"></Table>
     </div>
     <pagination :total="total"></pagination>
@@ -32,7 +29,8 @@ export default {
       keywords2: '',
       searching: false,
       isAdvancedSearch: false, // 是否为高级搜索
-      total: 100,
+      total: 1000,
+      loading: false,
       tableColumn: [
         {
           type: 'selection',
@@ -81,9 +79,26 @@ export default {
       ]
     }
   },
+  methods: {
+    searchData () {
+      this.searching = true
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+        this.searching = false
+      }, 1000)
+    }
+  },
   components: {
     TableToolsBar,
     Pagination
+  },
+  mounted () {
+    this.$loading.show(this, '正在获取数据...')
+    setTimeout(() => {
+      this.data = this.userData
+      this.$loading.remove(this)
+    }, 1000)
   }
 }
 </script>
@@ -91,21 +106,5 @@ export default {
 <style lang="less">
   @import '~styles/mixins';
   .standard-category{
-    .demo-spin-icon-load{
-      animation: ani-demo-spin 1s linear infinite;
-    }
-    @keyframes ani-demo-spin {
-      from { transform: rotate(0deg);}
-      50%  { transform: rotate(180deg);}
-      to   { transform: rotate(360deg);}
-    }
-    .demo-spin-col{
-      height: 100px;
-      position: relative;
-      border: 1px solid #eee;
-    }
-    @font-face {
-      .icon-font()
-    }
   }
 </style>
