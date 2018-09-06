8553e3055152f104fab34b6ee56fae17ad5051dd
yuzhong <740101231@qq.com>
2018-09-06 15:08:50 +0800

    流程页面开发

---
+++ adc-da-ui/src/main/laws_system_front_pc/src/pages/processCenter/pages/uncompletedProcess/components/transact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="transact">
+    <form>
+      流程编号
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'transact'
+}
+</script>
+
+<style lang="less">
+  #transact{}
+</style>
