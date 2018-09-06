cf512e872ad1ab9519f8ce777183e9a72936027a
liruohao <li754295543@163.com>
2018-09-06 19:20:58 +0800

    修改新增标准法规属性管理页面 删除新增Modal弹窗组件

--- adc-da-ui/src/main/laws_system_front_pc/src/pages/config/pages/regulationsManage/components/StandardCategory.vue
+++ adc-da-ui/src/main/laws_system_front_pc/src/pages/config/pages/regulationsManage/components/StandardCategory.vue
@@ -21,23 +21,27 @@
         <Button type="success" @click="openAdd">增加</Button>
         <Button type="warning" @click="openEdit">编辑</Button>
         <Button type="error">删除</Button>
-        <modal :show="modal" @cancel="modal = false" :title="title">
+        <!--显示模态框-->
+        <Modal v-model="modal" :title="title" :class="{ 'hide-modal-footer': modalType === 3 }" width="400">
           <!--编辑/查看-->
-          <div v-if="!isEdit">
+          <div v-if="modalType === 2 || modalType === 3">
             <Form :model="formLeft" label-position="right" :label-width="80">
               <FormItem label="标准">
-                <Input v-model="formLeft.input1" style="width: 300px"></Input>
+                <Input v-model="formLeft.input1" style="width: 200px" :disabled='modalType === 3'></Input>
               </FormItem>
               <FormItem label="描述">
-                <Input v-model="formLeft.input2" style="width: 300px"></Input>
+                <Input v-model="formLeft.input2" style="width: 200px" :disabled='modalType === 3'></Input>
+              </FormItem>
+              <FormItem label="创建人">
+                <Input v-model="formLeft.input3" style="width: 200px" :disabled='modalType === 3'></Input>
               </FormItem>
             </Form>
           </div>
           <!--新增-->
-          <div v-else>
+          <div v-else-if="modalType === 1">
             我是新增的内容
           </div>
-        </modal>
+        </Modal>
       </div>
     </table-tools-bar>
     <Table border ref="selection" :columns="columns1" :data="data1"></Table>
@@ -45,147 +49,150 @@
 </template>
 
 <script>
-  import tableToolsBar from 'pages/components/tableToolsBar'
-  import Modal from 'pages/components/Modal'
-  export default {
-    name: 'standard-classification',
-    data() {
-      return {
-        isEdit: false, // 是否为编辑
-        title:'',
-        formInline: {
-          option: '',
-          describe: '',
+import tableToolsBar from 'pages/components/tableToolsBar'
+export default {
+  name: 'standard-classification',
+  data () {
+    return {
+      modalType: '',
+      title: '',
+      formInline: {
+        option: '',
+        describe: ''
+      },
+      formLeft: {
+        input1: '',
+        input2: '',
+        input3: ''
+      },
+      formRight: {
+        input1: '',
+        input2: '',
+        input3: ''
+      },
+      modal: false,
+      value1: '',
+      value2: '',
+      columns1: [
+        {
+          type: 'selection',
+          width: 60,
+          align: 'center'
         },
-        formLeft: {
-          input1: '',
-          input2: '',
-          input3: ''
+        {
+          title: '选项',
+          key: 'option',
+          align: 'center'
         },
-        formRight: {
-          input1: '',
-          input2: '',
-          input3: ''
+        {
+          title: '描述',
+          key: 'describe',
+          width: 300,
+          align: 'center'
         },
-        modal: false,
-        value1: '',
-        value2: '',
-        columns1: [
-          {
-            type: 'selection',
-            width: 60,
-            align: 'center'
-          },
-          {
-            title: '选项',
-            key: 'option',
-            align: 'center',
-          },
-          {
-            title: '描述',
-            key: 'describe',
-            width: 300,
-            align: 'center'
-          },
-          {
-            title: '创建日期',
-            key: 'creationDate',
-            align: 'center'
-          },
-          {
-            title: '创建人',
-            key: 'founder',
-            align: 'center'
-          },
-          {
-            title: 'Action',
-            key: 'action',
-            width: 150,
-            align: 'center',
-            render: (h, params) => {
-              return h('div', [
-                h('Button', {
-                  props: {
-                    type: 'primary',
-                    size: 'small'
-                  },
-                  style: {
-                    marginRight: '5px'
-                  },
-                  on: {
-                    click: () => {
-                      this.viewData(params.index)
-                    }
+        {
+          title: '创建日期',
+          key: 'creationDate',
+          align: 'center'
+        },
+        {
+          title: '创建人',
+          key: 'founder',
+          align: 'center'
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          width: 150,
+          align: 'center',
+          render: (h, params) => {
+            return h('div', [
+              h('Button', {
+                props: {
+                  type: 'primary',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.viewData(params.index)
                   }
-                }, '查看')
-              ]);
-            }
+                }
+              }, '查看')
+            ])
           }
-        ],
-        data1: [
-          {
-            option: '中国标准',
-            describe: 'XXXXXXXXXXXXXXXXXXX',
-            founder: 'Mr.li',
-            creationDate: '2018-9-05'
-          },
-          {
-            option: '企业标准',
-            describe:'XXXXXXXXXXXXXXXXXXX',
-            founder: 'Mr.li',
-            creationDate: '2018-9-05'
-          },
-          {
-            option: '欧盟标准',
-            describe:'XXXXXXXXXXXXXXXXXXX',
-            founder: 'Mr.li',
-            creationDate: '2018-9-05'
-          },
-          {
-            option: '美国标准',
-            describe:'XXXXXXXXXXXXXXXXXXX',
-            founder: 'Mr.li',
-            creationDate: '2018-9-05'
-          }
-        ]
-      }
-    },
-    methods: {
-      handleQuery(){
-
-      },
-      openAdd(){
-        this.modal = true
-        this.isEdit = true
-        this.title=this.modalTitle
-      },
-      //编辑
-      openEdit(){
-        this.modal = true
-        this.isEdit = false
-        this.title='编辑标准'
-      },
-      //查看
-      viewData () {
-        this.modal = true
-        this.isEdit = false
-        this.title=this.modalTitle
-      }
-    },
-    components: {
-      tableToolsBar,
-      Modal
-    },
-    computed: {
-      modalTitle () {
-        return this.isEdit ? '新增标准' : '查看标准'
-      }
+        }
+      ],
+      data1: [
+        {
+          option: '中国标准',
+          describe: 'XXXXXXXXXXXXXXXXXXX',
+          founder: 'Mr.li',
+          creationDate: '2018-9-05'
+        },
+        {
+          option: '企业标准',
+          describe: 'XXXXXXXXXXXXXXXXXXX',
+          founder: 'Mr.li',
+          creationDate: '2018-9-05'
+        },
+        {
+          option: '欧盟标准',
+          describe: 'XXXXXXXXXXXXXXXXXXX',
+          founder: 'Mr.li',
+          creationDate: '2018-9-05'
+        },
+        {
+          option: '美国标准',
+          describe: 'XXXXXXXXXXXXXXXXXXX',
+          founder: 'Mr.li',
+          creationDate: '2018-9-05'
+        }
+      ]
     }
+  },
+  methods: {
+    handleQuery () {
+
+    },
+    // 新增
+    openAdd () {
+      this.modal = true
+      this.modalType = 1
+      this.title = '新增标准'
+    },
+    // 编辑
+    openEdit () {
+      this.modal = true
+      this.modalType = 2
+      this.title = '编辑标准'
+    },
+    // 查看
+    viewData () {
+      this.modal = true
+      this.modalType = 3
+      this.title = '查看标准'
+      // $('.ivu-modal-footer').addClass('isDisplay')
+    }
+  },
+  components: {
+    tableToolsBar
+  },
+  computed: {
   }
+}
 </script>
 
-<style lang="less" scoped>
-  .standard-classification {
+<style lang="less">
+  .standard-classification {}
+  .hide-modal-footer{
+    .ivu-modal-footer{
+      display: none;
+    }
   }
-
+  .unable-edit{
+    disabled:disabled
+  }
 </style>
