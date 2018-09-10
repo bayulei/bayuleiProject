2a8cd6ed797c7e0a96b42655f0766666ec3fc82b
liruohao <li754295543@163.com>
2018-09-10 11:22:34 +0800

    修改预警时间设置页面

--- adc-da-ui/src/main/laws_system_front_pc/src/pages/config/pages/warningTimeSetting/index.vue
+++ adc-da-ui/src/main/laws_system_front_pc/src/pages/config/pages/warningTimeSetting/index.vue
@@ -1,9 +1,19 @@
 <!-- 预警时间设置 -->
 <template>
   <div class="warning-time-setting">
-    <Select v-model="warningModel" style="width:200px">
-      <Option v-for="item in warningList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-    </Select>
+    <Form ref="warningForm" :model="warningForm" :rules="warningRule" :label-width="100" style="margin: 1rem auto">
+      <FormItem label="预警时间设置" prop="warning">
+        <Select v-model="warningForm.warning">
+          <Option v-for="item in warningList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
+      </FormItem>
+      <FormItem>
+        <div class="btn-group">
+          <Button type="primary" @click="handleSubmit('warningForm')">保存</Button>
+          <Button style="margin-left: 8px" @click="handleReset ('warningForm')">返回</Button>
+        </div>
+      </FormItem>
+    </Form>
   </div>
 </template>
 
@@ -12,6 +22,9 @@ export default {
   name: 'warning-time-setting',
   data () {
     return {
+      warningForm: {
+        warning: ''
+      },
       warningList: [
         {
           value: 'Three months',
@@ -30,7 +43,25 @@ export default {
           label: '两年'
         }
       ],
-      warningModel: ''
+      warningRule: {
+        warning: [
+          { required: true, message: '请设置预警时间', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          this.$Message.success('Success!')
+        } else {
+          this.$Message.error('Fail!')
+        }
+      })
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
     }
   }
 }
@@ -40,5 +71,10 @@ export default {
   .warning-time-setting{
     display: flex;
     background: #FFF;
+    .btn-group{
+      margin:1rem;
+    }
+    .btn-group Button{
+    }
   }
 </style>
