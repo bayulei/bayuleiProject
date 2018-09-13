a3b6a223d943f6ef83188b73035313ef78f9618d
duyunbao <1114808306@qq.com>
2018-09-13 16:34:17 +0800

    4.5.4.7企业标准制修订计划调整申请流程

--- adc-da-activiti/src/main/java/com/adc/da/activiti/controller/StandardAdjustProcessController.java
+++ adc-da-activiti/src/main/java/com/adc/da/activiti/controller/StandardAdjustProcessController.java
@@ -1,7 +1,7 @@
 package com.adc.da.activiti.controller;
 
-import com.adc.da.activiti.entity.VehicleApprovalEO;
 import com.adc.da.activiti.service.StandardAdjustProcessService;
+import com.adc.da.activiti.vo.StandardAdjustVO;
 import com.adc.da.util.http.ResponseMessage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,10 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 
 @RestController
@@ -25,20 +25,32 @@ public class StandardAdjustProcessController {
     /**
      *  企业标准制修订计划调整申请流程的流程图Key
      */
-    public  static  final String ProcessDefinitionKey = "StandardAdjustProcess";
+    public  static  final String STANDARD_ADJUST_KEY = "StandardAdjustProcess";
 
     @Autowired
     private StandardAdjustProcessService standardAdjustProcessService;
 
-   /* @ApiOperation(value = "标准编写部门发起流程")
-    @PostMapping("/submitOrSaveStandardApprovalProcess")
-    public ResponseMessage<String> submitOrSaveStandardApprovalProcess(@RequestBody VehicleApprovalEO vehicleApprovalEO, MultipartFile file
-            , String flag, String ProcessInstanceId) throws Exception {
+    @ApiOperation(value = "标准编写部门发起流程")
+    @PostMapping("/submitOrSaveStandardAdjustProcess")
+    public ResponseMessage<String> submitOrSaveStandardApprovalProcess(StandardAdjustVO standardAdjustVO) throws Exception {
 
-        //调用上传附件的接口
-        String url = "附件地址";
-        vehicleApprovalEO.setFileUrl(url);
-        return  vehicleApprovalService.submitOrSaveStandardApprovalProcess(vehicleApprovalEO,flag,ProcessInstanceId);
+        return  standardAdjustProcessService.submitOrSaveStandardApprovalProcess(standardAdjustVO);
 
-    }*/
+    }
+
+
+    @ApiOperation(value = "提交或退回")
+    @PostMapping ("/SubmitOrRetreatProcess")
+    public ResponseMessage<String> SubmitOrRetreatProcess(String flag, String taskId,String comment) throws Exception {
+
+        return  standardAdjustProcessService.SubmitOrRetreatProcess(flag,taskId,comment);
+
+    }
+
+
+    @ApiOperation(value = "根据任务id查询流程变量")
+    @PostMapping ("/queryProcessVariable")
+    public ResponseMessage<Map<String,Object>> queryProcessVariable(String taskId) throws Exception {
+        return standardAdjustProcessService.queryProcessVariable(taskId);
+    }
 }
