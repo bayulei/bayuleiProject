ad0405760e585977903d340e13ffd001b85ef95a
chenxiaoxi <chenxiaoxi@91isoft.com>
2018-09-14 11:28:24 +0800

    修改axios包，修改put方法，增加delete方法 增加vuex状态数据，增加全局样式

--- adc-da-ui/src/main/laws_system_front_pc/src/common/axios/index.js
+++ adc-da-ui/src/main/laws_system_front_pc/src/common/axios/index.js
@@ -210,6 +210,39 @@ module.exports = {
   },
 
   /**
+   * @description: put方法(json提交)
+   * @author: chenxiaoxi
+   * @date: 2018-09-06 13:28:37
+   */
+  putData (url, param, config, thenFun, exeFun) {
+    // 参数包含this
+    let _this = config._this || false
+    // 参数包含loading
+    let loading = config.loading || false
+    if (_this) {
+      _this[loading] = true
+    }
+    axios.put('/api/' + url, param).then(res => {
+      if (_this && loading) { _this[loading] = false }
+      if (res.data.ok !== undefined) {
+        let type = res.data.ok ? 'success' : 'warning'
+        if (_this) { _this.$Message[type](res.data.message) }
+        if (res.data.ok) {
+          thenFun.call(this, res.data)
+        }
+      }
+    }).catch(err => {
+      if (_this && loading) {
+        _this[loading] = false
+        _this.$Notice.error({
+          title: '错误',
+          desc: '网络连接错误'
+        })
+      }
+      exeFun.call(this, err)
+    })
+  },
+  /**
    * @description: delete方法
    * @author: chenxiaoxi
    * @date: 2018-09-14 09:55:30
