21efacffd2c04ac61fc20c2c6c9f686925786abc
liruohao <li754295543@163.com>
2018-09-24 22:06:14 +0800

    优化配置管理的页面

--- adc-da-ui/src/main/laws_system_front_pc/src/pages/personal/pages/feedback -feedback/index.vue
+++ adc-da-ui/src/main/laws_system_front_pc/src/pages/personal/pages/feedback -feedback/index.vue
@@ -1,12 +1,33 @@
 <template>
- <div id="index"></div>
+ <div id="index">
+   <UEditor :config=config ref="ueditor"></UEditor>
+   <Button type="primary" style="margin: 1rem 9rem">提交</Button>
+ </div>
 </template>
 
 <script>
 export default {
   name: 'index',
   data () {
-    return {}
+    return {
+      config: {
+        // 可以在此处定义工具栏的内容
+        toolbars: [
+          ['fullscreen', 'undo', 'redo', 'bold', 'indent', 'italic', 'underline', 'strikethrough', 'blockquote', 'simpleupload',
+            '|', 'selectall', 'formatmatch', 'horizontal', 'removeformat', 'time', 'date', 'inserttitle',
+            '|', 'cleardoc', 'fontfamily', 'fontsize', 'paragraph', 'link', 'searchreplace', 'justifyleft', 'justifyright', 'justifycenter', 'justifyjustify', 'forecolor', 'backcolor', // 背景色
+            '|']
+        ],
+        autoHeightEnabled: false,
+        autoFloatEnabled: true,
+        initialContent: '请输入内容', // 初始化编辑器的内容,也可以通过textarea/script给值，看官网例子
+        autoClearinitialContent: true, // 是否自动清除编辑器初始内容，注意：如果focus属性设置为true,这个也为真，那么编辑器一上来就会触发导致初始化的内容看不到了
+        initialFrameWidth: null,
+        initialFrameHeight: 450,
+        BaseUrl: '',
+        UEDITOR_HOME_URL: 'static/ueditor/'
+      }
+    }
   },
   methods: {},
   components: {},
